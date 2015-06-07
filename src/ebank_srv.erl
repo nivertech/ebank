2fbefb09b1cc9d50b85e376686f668c1656fd912
Zvi Avraham <zvi@nivertech.com>
2015-06-07 12:23:56 +0300

    add lager

--- src/ebank_srv.erl
+++ src/ebank_srv.erl
@@ -27,7 +27,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
-%% @doc unit in which account balance is held
+%% unit in which account balance is held
 -type currency() :: number().
 
 %%====================================================================
